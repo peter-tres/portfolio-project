359af73f33611fcfe62abc63c3ad6e9aa74911c0
peter-tres <motap95@gmail.com>
2025-06-12 12:47:57 -0700

    added navbar bg fade in

--- src/components/PNav.jsx
+++ src/components/PNav.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {useEffect, useState} from 'react'
 import './PNav.css'
 
 
 function PNav(){
+
+    const [scrolled, setScrolled] = useState(false);
+
+
+
+    useEffect(()=>{
+        const onScroll = () => {
+            const isScrolled = window.scrollY > 50.0
+            setScrolled(isScrolled);
+            console.log(isScrolled);
+            
+        };
+
+        window.addEventListener('scroll', onScroll);
+        
+        return () => window.removeEventListener('scroll', onScroll);
+
+    },
+    []);
+
+
     return(
-        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light pb-3">
+        <nav className={"navbar sticky-top navbar-expand-lg navbar-light bg-light pb-3 " + (scrolled? "scroll-bg": "transparent-bg")}>
         <div className="container-fluid d-flex justify-content-end">
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
